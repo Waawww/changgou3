9eb6beee427a8c462ec214f0ea7ce7fccd2ab8de
itcast <ontbibbb@gmail.com>
2020-05-05 22:47:39 +0800

    v1.13_5.5

---
+++ changgou_web/changgou_web_seckill/src/main/java/com/changgou/seckill/web/controller/SecKillGoodsController.java
@@ -0,0 +1,44 @@
+package com.changgou.seckill.web.controller;
+
+import com.changgou.util.DateUtil;
+import org.hibernate.validator.constraints.EAN;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/wseckillgoods")
+public class SecKillGoodsController {
+
+
+    //跳转秒杀首页
+    @RequestMapping("/toIndex")
+    public String toIndex(){
+        return "seckill-index";
+    }
+
+    //获取秒杀时间段集合信息
+    @RequestMapping("/timeMenus")
+    @ResponseBody
+    public List<String> dateMenus(){
+
+        //获取当前时间段相关的信息集合
+        List<Date> dateMenus = DateUtil.getDateMenus();
+        List<String> result = new ArrayList<>();
+
+        for (Date dateMenu : dateMenus) {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String format = simpleDateFormat.format(dateMenu);
+            result.add(format);
+        }
+
+        return result;
+    }
+
+
+}
